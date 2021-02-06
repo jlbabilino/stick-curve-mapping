b1a2df53ac53c3bfc9425f64e53b598a3975f8fb
BuildTools <unconfigured@null.spigotmc.org>
2021-02-06 11:28:18 -0500

    added a better test in io.github.jlbabilino.curvemapping.Main

--- io/github/jlbabilino/curvemapping/Main.java
+++ io/github/jlbabilino/curvemapping/Main.java
@@ -6,15 +6,16 @@ package io.github.jlbabilino.curvemapping;
  */
 public class Main {
     public static void main(String[] args) {
-        double[][] testPoints = { {-1.0,  1.0},
+        double[][] testPoints = { {-1.0, -0.2},
                                   {-0.5, -1.0},
-                                  { 0.0,  1.0},
-                                  { 0.5, -1.0},
-                                  { 1.0,  1.0} };
+                                  { 0.0,  0.3},
+                                  { 0.5, -0.8},
+                                  { 1.0,  0.9} };
         // double[][] testPoints = { {-0.6, -0.5},
         //                           { 0.0,  0.0},
         //                           { 0.6,  0.5} };
         SplineCurve curve = new SplineCurve();
+        curve.setSplineType(SplineType.SMOOTH);
         curve.setPoints(testPoints);
         // LinCurve curve = new LinCurve(0.0, 1.0, 0.05);
         curve.printPoints(curve.getCurvePoints(101));
