fcc99b09831bceb64adbb79f50f46acbe4b0871e
BuildTools <unconfigured@null.spigotmc.org>
2021-02-06 10:43:24 -0500

    1.0

---
+++ io/github/jlbabilino/curvemapping/Main.java
@@ -0,0 +1,22 @@
+package io.github.jlbabilino.curvemapping;
+/**
+ * Main class used for testing - delete eventually
+ *
+ * @version 2021-01-31
+ */
+public class Main {
+    public static void main(String[] args) {
+        double[][] testPoints = { {-1.0,  1.0},
+                                  {-0.5, -1.0},
+                                  { 0.0,  1.0},
+                                  { 0.5, -1.0},
+                                  { 1.0,  1.0} };
+        // double[][] testPoints = { {-0.6, -0.5},
+        //                           { 0.0,  0.0},
+        //                           { 0.6,  0.5} };
+        SplineCurve curve = new SplineCurve(SplineType.SMOOTH_AT_ENDS, testPoints, 0.0, 1.0, 0.0);
+        curve.printPoints(curve.getCurvePoints(101));
+        // LinCurve curve = new LinCurve(0.0, 1.0, 0.05);
+        // curve.printPoints(curve.getCurvePoints(101));
+    }
+}
---
+++ io/github/jlbabilino/curvemapping/SplineType.java
@@ -0,0 +1,9 @@
+package io.github.jlbabilino.curvemapping;
+/** Types of splines that can be used with <code>SplineCurve</code>.*/
+public enum SplineType {
+    LINEAR,
+    SMOOTH,
+    SMOOTH_AT_ENDS,
+    RIGHT_STEP,
+    LEFT_STEP
+}
